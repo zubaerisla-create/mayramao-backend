2daf0378733b0a0f158b333430a68199646c5310
abdullahalzubaerofficial <zubaer.steadfast@gmail.com>
2026-03-25 06:19:30 +0600

    update-buying

--- src/modules/user/user.interface.ts
+++ src/modules/user/user.interface.ts
@@ -29,7 +29,7 @@ export interface IUserProfile {
 
   // purchase simulation information (for frontend calculator)
   purchaseSimulation?: {
-    BuyingProductName?: string;
+ 
     purchaseAmount?: number;
     paymentType?: "PayInFull" | "Financing";
     loanDuration?: number;       // months, only if Financing
